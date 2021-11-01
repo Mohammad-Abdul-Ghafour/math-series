027a6b76c3a1f7865dbe57eb4bb41f57cdad2045
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2021-11-01 17:56:40 +0200

    Adding docstring to the functions

--- math_series/series.py
+++ math_series/series.py
@@ -1,7 +1,12 @@
 fibonacci_arr = [0,1]
 
 def fibonacci_series(num):
-    if num<=0:
+    """
+    A function that take a (num) as argument
+    and retun the nth number of fibonacci series
+    which start the series with 0 , 1
+    """
+    if num<0:
         return 0
     i = len(fibonacci_arr)
     if i>=num:
@@ -15,8 +20,13 @@ def fibonacci_series(num):
 
 lucas_arr = [2,1]
 def lucas_series(num):
+    """
+    A function that take a (num) as argument
+    and retun the nth number of lucas series
+    which start the series with 2 , 1
+    """
     if num<0:
-        return 0
+        return 2
     i = len(lucas_arr)
     if i>=num:
         return lucas_arr[num - 1]
@@ -28,7 +38,13 @@ def lucas_series(num):
             return lucas_series(num)
 
 def sum_series(num,ob1=0,ob2=1):
-    if num<=0:
+    """
+    A function that take a (num), (ob1) (ob2) as argument
+    and retun the nth number of a series
+    which start the series with ob1 , ob2
+    Note : ob1 = 0 , ob2 = 1 as default
+    """
+    if num<0:
         return ob1
     arr =[ob1,ob2]
     if num < len(arr):
@@ -40,3 +56,12 @@ def sum_series(num,ob1=0,ob2=1):
         else:
             ob1 , ob2 = ob2 , arr[2]
             return sum_series(num - 1,ob1,ob2)
+
+if __name__ == "__main__":
+    print("Please enter the nth number you ask for")
+    num = int(input(" > "))
+    print("Please enter the first numbers of the series")
+    ob1 = int(input(" > "))
+    print("Please enter the second numbers of the series")
+    ob2 = int(input(" > "))
+    print(sum_series(num,ob1,ob2)) 
